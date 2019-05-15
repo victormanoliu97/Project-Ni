a7856549cb7b7200e3ea0aa500bebbdaf3fe336f
Paius Emanuel Andrei <pandaismyname1@gmail.com>
2019-05-15 04:32:36 +0300

    Implemented Tag Repository

--- Ni/Ni/Core/Repositories/ITagRepository.cs
+++ Ni/Ni/Core/Repositories/ITagRepository.cs
@@ -1,7 +1,11 @@
+using Ni.Core.Entities;
+using System.Collections.Generic;
+
 namespace Ni.Repositories
 {
     public interface ITagRepository
     {
-        
+        List<Tag> GetByPostId(int postId);
+        void AddToPost(int postId, string tag);
     }
 }
--- Ni/Ni/Infrastructure/Repositories/TagRepository.cs
+++ Ni/Ni/Infrastructure/Repositories/TagRepository.cs
@@ -1,7 +1,36 @@
+using System.Collections.Generic;
+using Ni.Core.Entities;
+using Ni.Repositories;
+using System.Linq;
+
 namespace Ni.Infrastructure.Repositories
 {
-    public class TagRepository
+    public class TagRepository : ITagRepository
     {
-        
+        private AppDbContext _appDbContext;
+
+        public TagRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void AddToPost(int postId, string tag)
+        {
+            Tag newTag = new Tag()
+            {
+                PostId = postId,
+                Title = tag,
+            };
+            _appDbContext.Tags.Add(newTag);
+            _appDbContext.SaveChanges();
+        }
+
+        public List<Tag> GetByPostId(int postId)
+        {
+            var query = from entity in _appDbContext.Tags
+                        where entity.PostId == postId
+                        select entity;
+            return query.ToList();
+        }
     }
 }
